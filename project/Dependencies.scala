3ec817b2895d0b0a7d2d774fe90ad90ebda9ef09
Peter Vlugter <pvlugter@gmail.com>
2017-05-16 10:33:38 +1200

    Update to paradox 0.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Version {
   val foundation = "6.3.1"
   val prettify   = "4-Mar-2013-1"
-  val sbtParadox = "0.2.11-SNAPSHOT" // todo: use published version
+  val sbtParadox = "0.2.11"
 }
 
 object Library {
