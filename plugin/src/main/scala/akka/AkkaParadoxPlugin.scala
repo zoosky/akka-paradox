793fe99ede1a2259eb88f6ea4155f0d04be67ba7
Peter Vlugter <pvlugter@gmail.com>
2017-05-15 16:46:14 +1200

    Add sidenote directive and styling

--- plugin/src/main/scala/akka/AkkaParadoxPlugin.scala
+++ plugin/src/main/scala/akka/AkkaParadoxPlugin.scala
@@ -2,7 +2,10 @@ package akka
 
 import sbt._
 import sbt.Keys._
+import com.lightbend.paradox.markdown.{ ContainerBlockDirective, Directive, Writer }
 import com.lightbend.paradox.sbt.ParadoxPlugin
+import org.pegdown.Printer
+import org.pegdown.ast.{ DirectiveNode, Visitor }
 
 object AkkaParadoxPlugin extends AutoPlugin {
 
@@ -20,11 +23,23 @@ object AkkaParadoxPlugin extends AutoPlugin {
     paradoxTheme := Some("com.lightbend.akka" % "paradox-theme-akka" % version),
     paradoxNavigationDepth := 1,
     paradoxNavigationExpandDepth := Some(1),
-    paradoxNavigationIncludeHeaders := true
+    paradoxNavigationIncludeHeaders := true,
+    paradoxDirectives += SidenoteDirective
   )
 
   def akkaParadoxSettings(config: Configuration): Seq[Setting[_]] = akkaParadoxGlobalSettings ++ inConfig(config)(Seq(
     // scoped settings here
   ))
+
+  object SidenoteDirective extends ContainerBlockDirective("sidenote") with (Writer.Context => Directive) {
+    def apply(context: Writer.Context): Directive = SidenoteDirective
+
+    def render(node: DirectiveNode, visitor: Visitor, printer: Printer): Unit = {
+      val classes = node.attributes.classesString
+      printer.print(s"""<div class="sidenote $classes">""")
+      node.contentsNode.accept(visitor)
+      printer.print("""</div>""")
+    }
+  }
 
 }
