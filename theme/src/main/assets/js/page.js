ba8563e10fd1c07d2bb6f44120d428a1ddfa6ef7
Peter Vlugter <pvlugter@gmail.com>
2017-05-17 15:15:18 +1200

    Add box style for TOCs of depth 1

--- theme/src/main/assets/js/page.js
+++ theme/src/main/assets/js/page.js
@@ -3,18 +3,24 @@ $(function() {
   // close the overlay navigation when header links are clicked
   $(".overlay-nav .nav-toc a.header, .overlay-nav .nav-toc a.active.page").attr("data-toggle", "underlay overlay");
 
-  // modify the layout of section TOCs to be in a block grid with equal heights
-  // force block grid with .blocks class, force regular list style with .list class
-  // default to block style for TOCs with depth > 1, list style for depth == 1
+  // TOCs support three styles:
+  //   - box: wrap in a shadowed box and apply TOC styling
+  //   - blocks: section TOCs in boxes in a block grid with equal heights
+  //   - list: regular list of links
   var tocs = $(".page-content .toc");
   tocs.each(function() {
     var toc = $(this);
-    var list = toc.children("ul");
-    var shallow = list.children("li").has("ul").length == 0;
-    if (toc.hasClass("blocks") || !(toc.hasClass("list") || shallow)) {
+    // if there's no style already set then add .box for TOCs of depth 1 otherwise .blocks
+    if (!(toc.hasClass("blocks") || toc.hasClass("box") || toc.hasClass("list"))) {
+      toc.addClass((toc.children("ul").children("li").has("ul").length == 0) ? "box" : "blocks");
+    }
+    if (toc.hasClass("blocks")) {
+      var list = toc.children("ul");
       list.addClass("row medium-up-2 large-up-3 toc-grid");
-      list.children("li").addClass("column column-block toc-block").attr("data-equalizer-watch", "").wrapInner( "<div class='toc-box'></div>");
+      list.children("li").addClass("column column-block toc-block").attr("data-equalizer-watch", "").wrapInner("<div class='toc-box'></div>");
       new Foundation.Equalizer(list, { equalizeByRow: true, equalizeOn: "medium" });
+    } else if (toc.hasClass("box")) {
+      toc.wrapInner("<div class='toc-box'></div>");
     }
   });
 
